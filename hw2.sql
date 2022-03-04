eddcc5c182cf60f8720126e1869f4676c8847d22
Vladislav <97437680+JokerQA@users.noreply.github.com>
2022-03-05 03:57:08 +0500

    Change utf8

--- hw2.sql
+++ hw2.sql
@@ -1,20 +1,20 @@
-/*1.Создать таблицу employees*/
+/*1.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employees*/
 create table employess(
 	id serial primary key,
 	employee_name varchar(50) not null
 )
 
-/*2.Наполнить таблицу employee 70 строками. Здесь я добавил одну строку, а остальные 70 записей сгенерировал python скриптом*/
+/*2.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee 70 СЃС‚СЂРѕРєР°РјРё. Р—РґРµСЃСЊ СЏ РґРѕР±Р°РІРёР» РѕРґРЅСѓ СЃС‚СЂРѕРєСѓ, Р° РѕСЃС‚Р°Р»СЊРЅС‹Рµ 70 Р·Р°РїРёСЃРµР№ СЃРіРµРЅРµСЂРёСЂРѕРІР°Р» python СЃРєСЂРёРїС‚РѕРј*/
 insert into employess(employee_name)
 values('Vladislav')
 
-/*3.Создать таблицу salary*/
+/*3.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ salary*/
 create table salary(
 id serial primary key,
 monthly_salary int not null
 )
 
-/*4.Наполнить таблицу salary 15 строками*/
+/*4.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ salary 15 СЃС‚СЂРѕРєР°РјРё*/
 insert into salary(monthly_salary)
 values(1000),
       (1100),
@@ -32,28 +32,28 @@ values(1000),
       (2400),
       (2500)
  
-/*5.Создать таблицу employee_salary*/
+/*5.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary*/
 create table employee_salary(
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null
 )
 
-/*6.Наполнить таблицу employee_salary 40 строками. Здесь я вновь воспользовался python скриптом и сгенерил 40 записей*/
+/*6.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ employee_salary 40 СЃС‚СЂРѕРєР°РјРё. Р—РґРµСЃСЊ СЏ РІРЅРѕРІСЊ РІРѕСЃРїРѕР»СЊР·РѕРІР°Р»СЃСЏ python СЃРєСЂРёРїС‚РѕРј Рё СЃРіРµРЅРµСЂРёР» 40 Р·Р°РїРёСЃРµР№*/
 insert into employee_salary(employee_id,salary_id)
 values(82,4)
 
-/*7.Создать таблицу roles*/
+/*7.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles*/
 create table roles(
 id serial primary key,
 role_name int not null unique
 )
 
-/*8.Поменять тип столба role_name с int на varchar(30)*/
+/*8.РџРѕРјРµРЅСЏС‚СЊ С‚РёРї СЃС‚РѕР»Р±Р° role_name СЃ int РЅР° varchar(30)*/
 alter table roles 
 alter column role_name type varchar(30)
 
-/*9.Наполнить таблицу roles 20 строками*/
+/*9.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles 20 СЃС‚СЂРѕРєР°РјРё*/
 insert into roles(role_name)
 values('Junior Python developer'),
       ('Middle Python developer'),
@@ -76,7 +76,7 @@ values('Junior Python developer'),
       ('Middle Automation QA engineer'),
       ('Senior Automation QA engineer')
 
-/*10.Создать таблицу roles_employee*/
+/*10.РЎРѕР·РґР°С‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee*/
 create table roles_employee(
 id serial primary key,
 employee_id int not null unique,
@@ -87,9 +87,8 @@ foreign key (role_id)
 	references roles(id)
 )
 
-/*11.Наполнить таблицу roles_employee 40 строками. 1 запись вручную, остальные python скриптом*/
+/*11.РќР°РїРѕР»РЅРёС‚СЊ С‚Р°Р±Р»РёС†Сѓ roles_employee 40 СЃС‚СЂРѕРєР°РјРё. 1 Р·Р°РїРёСЃСЊ РІСЂСѓС‡РЅСѓСЋ, РѕСЃС‚Р°Р»СЊРЅС‹Рµ python СЃРєСЂРёРїС‚РѕРј*/
 insert into roles_employee (employee_id,role_id)
 values(1,1)
 
-      
 
